d2e17a6d5b8ab28059f820a77642800260a2e6c7
黎辰 夏 <2471570456@qq.com>
2022-11-25 16:59:51 +0800

    函数

---
+++ Bilibili/计算闰年.1.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int is_leap_year(int n)
+{
+	if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int i = 0;
+	for (i = 1000; i < 2000; i++)
+	{
+		if (is_leap_year(i) == 1)
+			printf("%d ", i);
+	}
+	return 0;
+}
